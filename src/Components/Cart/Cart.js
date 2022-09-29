0aff087e9d98c08f8d3c389c162241416eb9367a
lutfullahillabib <lutfullahillabib@gmail.com>
2022-09-29 16:52:44 +0600

    toasts done

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,7 +4,7 @@ import './Cart.css'
 
 import Swal from 'sweetalert2';
 
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -30,14 +30,42 @@ const Cart = ({ timer }) => {
     console.log(timer)
 
     const toastsss = () => {
-        Swal.fire(
-            'Congratulation..!!',
-            'Activity Completed.!!',
-            'success'
-        )
+
+        if (timer > 0) {
+            Swal.fire(
+                'Congratulation..!!',
+                `${timer} mins,
+                Activity Completed.!!`,
+                'success'
+            )
+        }
+        else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...!!!',
+                text: `${timer} mins`,
+                footer: 'Please, select & do your exercise..!!'
+            })
+        }
+
     }
 
-    const notify = (bTime) => { toast(`Break Time: ${bTime}`) };
+    // const notify = (bTime) => { toast(`Break Time: ${bTime} secs`) };
+
+    const notify = (bTime) => {
+        toast.info(`Break Time: ${bTime} secs`, {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'dark',
+            transition: Zoom
+        });
+    }
+
 
     return (
         <div className='text-white bg-black cus-shadow p-3 rounded sticky-lg-top my-3 my-lg-0'>
@@ -74,11 +102,11 @@ const Cart = ({ timer }) => {
                     Add A Break
                 </h4>
                 <div className='pb-2 px-1 text-center'>
-                    <button onClick={() => { breaksTimeHandle(10); notify(10) }} className='btn bg-color text-white rounded-5 m-1'>10s</button>
-                    <button onClick={() => { breaksTimeHandle(20); notify(20) }} className='btn  bg-color text-white rounded-5 m-1'>20s</button>
-                    <button onClick={() => { breaksTimeHandle(30); notify(30) }} className='btn  bg-color text-white rounded-5 m-1'>30s</button>
-                    <button onClick={() => { breaksTimeHandle(40); notify(40) }} className='btn  bg-color text-white rounded-5 m-1'>40s</button>
-                    <button onClick={() => { breaksTimeHandle(50); notify(50) }} className='btn  bg-color text-white rounded-5 m-1'>50s</button>
+                    <button onClick={() => { breaksTimeHandle(10); notify(10) }} className='btn bg-color text-white rounded-5 m-1'>10 s</button>
+                    <button onClick={() => { breaksTimeHandle(20); notify(20) }} className='btn  bg-color text-white rounded-5 m-1'>20 s</button>
+                    <button onClick={() => { breaksTimeHandle(30); notify(30) }} className='btn  bg-color text-white rounded-5 m-1'>30 s</button>
+                    <button onClick={() => { breaksTimeHandle(40); notify(40) }} className='btn  bg-color text-white rounded-5 m-1'>40 s</button>
+                    <button onClick={() => { breaksTimeHandle(50); notify(50) }} className='btn  bg-color text-white rounded-5 m-1'>50 s</button>
                 </div>
             </div>
 
@@ -87,16 +115,27 @@ const Cart = ({ timer }) => {
                 <h4 className='text-center'>Exercise Details</h4>
                 <div className='d-flex justify-content-between bg-color pb-1 p-2 my-2 rounded align-items-center'>
                     <h5>Exercise Time</h5>
-                    <h5>{timer}h</h5>
+                    <h5><span className='text-primary fw-bolder'>{timer}</span> mins</h5>
                 </div>
 
                 <div className='d-flex justify-content-between bg-color pb-1 p-2 my-2 rounded align-items-center'>
                     <h5>Break Time</h5>
-                    <h5>{breaks}s</h5>
+                    <h5><span className='text-primary fw-bolder'>{breaks}</span> secs</h5>
                 </div>
 
                 <button onClick={toastsss} className='btn btn-light d-block mx-auto mt-4'>Activity Completed</button>
-                <ToastContainer />
+                {/* <ToastContainer /> */}
+                <ToastContainer
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                />
 
             </div>
         </div>
